3e045a48cbd2f843468d618dc210b20171c91738
leonelrojas17bs-cmyk <leonelrojas17bs@gmail.com>
2025-11-05 20:07:08 -0300

    .

---
+++ Ejerciciosenclase/ejerciciocumluca.py
@@ -0,0 +1,5 @@
+letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+patente = input("Ingrese la patente en formato AAA123: ").upper()
+corrimiento = int(input("Ingrese el corrimiento: "))
+for patente in range(0, 6): 
+    f)
